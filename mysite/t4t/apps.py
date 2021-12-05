51a9c13d86b66559bf67adcb77a40ee3d4915dc4
Gregg Riess <businessman8994@gmail.com>
2021-12-05 10:58:07 -0500

    Adding Django Imports

--- mysite/t4t/apps.py
+++ mysite/t4t/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class T4TConfig(AppConfig):
+class T4tConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 't4t'
--- mysite/t4t/views.py
+++ mysite/t4t/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
-
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from .models import Post
+from .forms import EditorForm
 # Create your views here.
