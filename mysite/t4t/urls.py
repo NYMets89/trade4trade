d03582df1ac71dc6963e0f51ce18f93c57aee26e
Gregg Riess <businessman8994@gmail.com>
2021-12-05 13:09:58 -0500

    Update Homepage, urls

--- mysite/t4t/urls.py
+++ mysite/t4t/urls.py
@@ -1,2 +1,10 @@
 from django.urls import path
 from . import views
+
+urlpatterns = [
+    # home/
+    path('', views.home, name='home'),
+    # blog/edit/<int:post_id>
+#     path('edit/<int:post_id>', views.edit, name='edit'),
+#     path('create/', views.create, name='create'),
+]
--- mysite/t4t/views.py
+++ mysite/t4t/views.py
@@ -4,3 +4,7 @@ from django.urls import reverse
 from .models import Post
 from .forms import EditorForm
 # Create your views here.
+
+def home(request):
+    # get QuerySet object containing posts in descending order of post_id
+    return render(request=request, template_name='home.html')
