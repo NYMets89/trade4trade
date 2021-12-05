71c3227dd6b75773ac10784bae1d62c1ae18203b
Gregg Riess <businessman8994@gmail.com>
2021-12-05 12:27:45 -0500

    Adding Models

--- mysite/t4t/models.py
+++ mysite/t4t/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    category_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+class Tag(models.Model):
+    tag_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+
+class Post(models.Model):
+    post_id = models.AutoField(primary_key=True)
+    img_link = models.URLField(max_length=400)
+    title =  models.CharField(max_length=255)
+    body = models.TextField()
+    tags = models.ManyToManyField(Tag)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    comment_id = models.AutoField(primary_key=True)
+    comment = models.CharField(max_length=255)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
