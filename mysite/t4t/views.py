bee1a944da9d9e3932421115717e552d2fd5bb84
Gregg Riess <businessman8994@gmail.com>
2021-12-19 14:08:53 -0500

    Add CRUD features to Posts

--- mysite/t4t/views.py
+++ mysite/t4t/views.py
@@ -17,7 +17,7 @@ def skillspage(request, category_id):
     # get QuerySet object containing posts in descending order of post_id
     category = Category.objects.get(pk=category_id)
     posts = Post.objects.filter(category=category_id).order_by('-post_id')
-###   NEED TO FILTER BY CATEGORY_ID    ### 
+
     if request.method == 'GET':
         form = EditorForm()
         return render(request=request, template_name='skills.html', context={ 'form': form, "posts": posts, "category":category })
@@ -35,7 +35,8 @@ def skillspage(request, category_id):
 
         return redirect('skillspage', category_id=category.category_id)
 
-def edit(request, post_id):
+def edit(request, post_id, category_id):
+    
     if request.method == 'GET':
         # get Post object by its post_id
         post = Post.objects.get(pk=post_id)
@@ -45,7 +46,7 @@ def edit(request, post_id):
         #     tags.append(tag.tag_id)
         # pre-populate form with values of the post
         form = EditorForm(initial={ 'title': post.title, 'body': post.body})
-        return render(request=request, template_name='edit.html', context={ 'form': form, 'id': post_id })
+        return render(request=request, template_name='edit.html', context={ 'form': form, 'post_id': post_id, 'category_id':category_id })
     if request.method == 'POST':    
         # capture POST data as EditorForm instance
         form = EditorForm(request.POST)
@@ -56,19 +57,19 @@ def edit(request, post_id):
                 # get cleaned data from form
                 title = form.cleaned_data['title']
                 body = form.cleaned_data['body']
-                tags = form.cleaned_data['tags']
+                # tags = form.cleaned_data['tags']
                 # filter QuerySet object by post_id
                 posts = Post.objects.filter(pk=post_id)
                 # update QuerySet object with cleaned title, body, img_link
                 posts.update(title=title, body=body)
                 # set cleaned tags to ManyRelatedManager object
-                posts[0].tags.set(tags)
+                # posts[0].tags.set(tags)
             # if form was submitted by clicking Delete
             elif 'delete' in request.POST:
                 # filter QuerySet object by post_id and delete it
                 Post.objects.filter(pk=post_id).delete()
         # redirect to 'blog/'
-        return HttpResponseRedirect(reverse('home'))
+        return redirect('skillspage', category_id=category_id)
 
 def create(request):
     if request.method == 'GET':
@@ -81,10 +82,10 @@ def create(request):
         if form.is_valid():
             title = form.cleaned_data['title']
             body = form.cleaned_data['body']
-            tags = form.cleaned_data['tags']
+            # tags = form.cleaned_data['tags']
             
-            post = Post.objects.create(title=title, body=body)
-            post.tags.set(tags) 
+            #post = Post.objects.create(title=title, body=body)
+            # post.tags.set(tags) 
 
         # redirect to 'blog/'
         return HttpResponseRedirect(reverse('home'))
