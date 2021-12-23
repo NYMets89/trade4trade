3c421fb6aefa3a1453eb583c4f4500b7edd4b4cd
Gregg Riess <businessman8994@gmail.com>
2021-12-22 19:00:33 -0500

    Remove tags temporary

--- mysite/t4t/views.py
+++ mysite/t4t/views.py
@@ -57,13 +57,13 @@ def edit(request, post_id, category_id):
                 # get cleaned data from form
                 title = form.cleaned_data['title']
                 body = form.cleaned_data['body']
-                tags = form.cleaned_data['tags']
+                #tags = form.cleaned_data['tags']
                 # filter QuerySet object by post_id
                 posts = Post.objects.filter(pk=post_id)
                 # update QuerySet object with cleaned title, body, img_link
                 posts.update(title=title, body=body)
                 # set cleaned tags to ManyRelatedManager object
-                posts[0].tags.set(tags)
+                #posts[0].tags.set(tags)
             # if form was submitted by clicking Delete
             elif 'delete' in request.POST:
                 # filter QuerySet object by post_id and delete it
@@ -82,10 +82,10 @@ def create(request):
         if form.is_valid():
             title = form.cleaned_data['title']
             body = form.cleaned_data['body']
-            tags = form.cleaned_data['tags']
+            #tags = form.cleaned_data['tags']
             
             post = Post.objects.create(title=title, body=body)
-            post.tags.set(tags) 
+            #post.tags.set(tags) 
 
         # redirect to 'blog/'
         return HttpResponseRedirect(reverse('home'))
